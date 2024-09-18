24e4409f8df6a64825c7f9171ac6a2e06c559317
Jonathan Visona <Jonathan.Visona@proaxys.com>
2024-09-18 01:03:04 -0500

    Added Multiset to objects_adt.py

--- montague.py
+++ montague.py
@@ -19,11 +19,21 @@ def main():
     # --------------------------------
 
     """
+    # Set example
     set1 = ADT.Set( "TestSet", {3,4,5,1}, "whole" )
     for each_value in list( set1.value ):
         print( "'" + str( each_value ) + "'", end= " " )
     print()
-    print( set1.toString() )
+    print( set1.to_string() )
+    """
+    """
+    # Multiset example
+    mset1 = ADT.Multiset( "TestSet", [3,4,5,1,1], "whole" )
+    #for each_value in list( mset1.value ):
+    #    print( "'" + str( each_value ) + "'", end= " " )
+    #print()
+    print( mset1.to_stringified_list() )
+    #print( mset1.to_string() )
     """
 
     # --------------------------------
@@ -38,7 +48,7 @@ def main():
         },
         "DO Sobj_manE STUFF"
     )
-    #print( proc1.toString() )
+    #print( proc1.to_string() )
     #print( proc1.toPython() )
     """
 
@@ -49,13 +59,13 @@ def main():
     """
     var1 = LOGIC.Variable( "x", "length" )
     var1.setVal( set1 )
-    print( var1.toString() )
+    print( var1.to_string() )
     """
 
     # --------------------------------
     # objects_text
     # --------------------------------
-    #"""
+    """
     prop1 = TEXT.Proposition( "P1", "All men are mortal.", "english" )
     prop2 = TEXT.Proposition( "P2", "Socrates is a man.", "english" )
     concl = TEXT.Proposition( "C", "Socrates is mortal.", "english" )
@@ -63,22 +73,22 @@ def main():
     obj_man.add_object( prop2 )
     obj_man.add_object( concl )
 
-    """
+
     arg1 = TEXT.Argument( "syllogism", [prop1, prop2], concl )
     obj_man.add_object( arg1 )
     print( arg1.toMultiString() )
-    """
+
 
     #prop1.regen_uuid()
-    #print( prop1.toString() )
-    #print( prop2.toString() )
+    #print( prop1.to_string() )
+    #print( prop2.to_string() )
     # print( obj_man.list_keys() )
     #print( obj_man.get_val_by_key( prop2.get_uuid_str() ) )
     #print( obj_man.get_size() )
     #obj_man.pop_object( prop2 )
     #print( obj_man.object_list )
     #print( obj_man.list_keys() )
-    #"""
+    """
 
 # if this module is called first, call the main function
 if __name__ == "__main__":
--- objects_adt.py
+++ objects_adt.py
@@ -23,3 +23,29 @@ class Set( Object ):
         return f"{base_string} " \
             f"value={self.__value} " \
             f"type={self.__type}"
+    
+@dataclass
+class Multiset( Object ):
+    __value: list = field( default_factory=list )
+    __type: str = ""
+
+    def __init__ ( self, name: str = "", value: list = [], type: str = "" ):
+        super().__init__( name )
+        self.__value = value
+        self.__type = type
+
+    @property
+    def value( self ):
+        return self.__value
+    @value.setter
+    def value( self, value: list ):
+        self.__value = value
+
+    def to_stringified_list( self ):
+        return str( self.__value )
+
+    def to_string( self ):
+        base_string = super().toString()
+        return f"{base_string} " \
+            f"value={self.__value} " \
+            f"type={self.__type}"
